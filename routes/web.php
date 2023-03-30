5285b9a03e90642f9264d9e3b50d867732d2d231
apollo <Vladislavvitalevich@gmail.com>
2023-03-30 09:00:58 +0300

    add_form

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/form/first', 'FormController@index')->name('form-1');
